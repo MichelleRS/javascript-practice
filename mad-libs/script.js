4694e341ee7c53c11f7c83fdb44dd40ba8be16d0
Michelle Ross <miross629@gmail.com>
2023-03-16 14:35:51 -0700

    Display results on form submit, TODO: fix name input display

--- mad-libs/script.js
+++ mad-libs/script.js
@@ -1,10 +1,10 @@
 function getAndSetValues() {
   // initialize variables get input value
-  const nameInput = document.getElementById("nameInput");
-  const animalInput = document.getElementById("animalInput");
-  const vegetableInput = document.getElementById("vegetableInput");
-  const musicGenreInput = document.getElementById("musicGenreInput");
-  const movieTitleInput = document.getElementById("movieTitleInput");
+  const nameInput = document.getElementById("nameInput").value;
+  const animalInput = document.getElementById("animalInput").value;
+  const vegetableInput = document.getElementById("vegetableInput").value;
+  const musicGenreInput = document.getElementById("musicGenreInput").value;
+  const movieTitleInput = document.getElementById("movieTitleInput").value;
 
   // set input values in their spans
   document.querySelector("span#name").innerText = nameInput;
@@ -14,4 +14,22 @@ function getAndSetValues() {
   document.querySelector("span#movie").innerText = movieTitleInput;
 }
 
-// display results
+// display results on form submit
+function setFormSubmissionEventHandler() {
+  let form = document.querySelector("form");
+  form.onsubmit = function (e) {
+    // prevent default behavior of refreshing page
+    e.preventDefault();
+
+    // call get and set values
+    getAndSetValues();
+
+    // remove .visually-hidden attribute value from results section
+    const results = document.querySelector("section#results");
+    results.classList.remove("visually-hidden");
+  };
+}
+
+window.onload = function () {
+  setFormSubmissionEventHandler();
+};
