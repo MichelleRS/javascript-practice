a09cb0b3173e3ff3be0a0a7f746b6678c9364492
Michelle Ross <miross629@gmail.com>
2023-03-20 12:08:38 -0700

    Display 0 in front of numbers less than 10

--- ticking-clock/script.js
+++ ticking-clock/script.js
@@ -17,6 +17,13 @@ function displayTime() {
   let mins = currentDate.getMinutes();
   let secs = currentDate.getSeconds();
 
+  // display 0 in front of numbers less than 10
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  // TODO: display am/pm
+
   // initialize a string to hold hrs, mins, and secs
   let timeString = hrs + ":" + mins + ":" + secs;
 
