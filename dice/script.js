aa184e9e2739b28ba83234a3b3092bf2e72cce69
Michelle Ross <miross629@gmail.com>
2023-03-20 15:40:25 -0700

    On button click, display a random number: 1-6

---
+++ dice/script.js
@@ -0,0 +1,9 @@
+// get DOM elements
+const rollButton = document.getElementById("roll");
+const numberRolled = document.getElementById("num");
+
+// display number rolled on button click
+rollButton.addEventListener("click", () => {
+  // set numberRolled to a random number: 1-6
+  numberRolled.textContent = Math.floor(Math.random() * 6);
+});
