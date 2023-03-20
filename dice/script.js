b4d41d142fd366d2c73690b92a6f6df966e6d1e0
Michelle Ross <miross629@gmail.com>
2023-03-20 16:06:25 -0700

    Fix number rolled to be 1-6

--- dice/script.js
+++ dice/script.js
@@ -5,5 +5,5 @@ const numberRolled = document.getElementById("num");
 // display number rolled on button click
 rollButton.addEventListener("click", () => {
   // set numberRolled to a random number: 1-6
-  numberRolled.textContent = Math.floor(Math.random() * 6);
+  numberRolled.textContent = Math.floor(Math.random() * 6) + 1;
 });
