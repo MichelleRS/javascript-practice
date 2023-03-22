785e71cf551b7eed747e91bf5026c6b96fc8fec9
Michelle Ross <miross629@gmail.com>
2023-03-22 12:26:25 -0700

    Display running total on Add to Total button click

---
+++ totalizer/script.js
@@ -0,0 +1,28 @@
+// get DOM elements
+const display = document.getElementById("displayText");
+const totalDisplay = document.getElementById("totalValue");
+const inputValue = document.getElementById("inputValue");
+const buttonAdd = document.getElementById("buttonAdd");
+const buttonReset = document.getElementById("buttonReset");
+
+// state
+let total = 0;
+
+// events
+
+// add total
+buttonAdd.addEventListener("click", (e) => {
+  // prevent page from refreshing
+  e.preventDefault();
+
+  // get input value and convert to a number
+  let value = Number(inputValue.value);
+
+  // add value to total
+  total = total + value;
+
+  // display current total value
+  totalDisplay.textContent = total;
+});
+
+// reset total
