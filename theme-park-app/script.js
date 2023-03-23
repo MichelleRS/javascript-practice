59e70ff4e43e55feaf9ad08d0c6a8964b5008aaf
Michelle Ross <miross629@gmail.com>
2023-03-23 11:16:09 -0700

    Refactor event listener with switch construction

--- theme-park-app/script.js
+++ theme-park-app/script.js
@@ -18,57 +18,59 @@ form.addEventListener("submit", (e) => {
   // error handling
   if (rideNumber > 5) {
     answerPar.textContent =
-      "The ride number you entered does not exist. Check the list of rides and try again.";
+      "The ride number entered does not exist. Check the list of rides and try again.";
   }
 
-  // check scenic river cruise
-  if (rideNumber === 1) {
-    answerPar.textContent =
-      "Yes, the age entered may ride the Scenic River Cruise. It is open to all ages.";
-  }
+  switch (rideNumber) {
+    // check scenic river cruise
+    case 1:
+      answerPar.textContent =
+        "Yes, the age entered may ride the Scenic River Cruise. It is open to all ages.";
+      break;
 
-  // check carnival carousel
-  if (rideNumber === 2) {
-    if (age < 3) {
-      answerPar.textContent =
-        "The age entered is not permitted to ride the Carnival Carousel. It is open to those 3 and older.";
-    } else {
-      answerPar.textContent =
-        "Yes, the age entered is permitted to ride the Carnival Carousel. It is open to those 3 and older.";
-    }
-  }
+    // check carnival carousel
+    case 2:
+      if (age < 3) {
+        answerPar.textContent =
+          "The age entered is not permitted to ride the Carnival Carousel. It is open to those 3 and older.";
+      } else {
+        answerPar.textContent =
+          "Yes, the age entered is permitted to ride the Carnival Carousel. It is open to those 3 and older.";
+      }
+      break;
 
-  // check jungle adventure water splash
-  if (rideNumber === 3) {
-    if (age < 6) {
-      answerPar.textContent =
-        "The age entered is not permitted to ride the Jungle Adventure Water Splash. It is open to those 6 and older.";
-    } else {
-      answerPar.textContent =
-        "Yes, the age entered may ride the Jungle Adventure Water Splash. It is open to those 6 and older.";
-    }
-  }
+    // check jungle adventure water splash
+    case 3:
+      if (age < 6) {
+        answerPar.textContent =
+          "The age entered is not permitted to ride the Jungle Adventure Water Splash. It is open to those 6 and older.";
+      } else {
+        answerPar.textContent =
+          "Yes, the age entered may ride the Jungle Adventure Water Splash. It is open to those 6 and older.";
+      }
+      break;
 
-  // check downhill mountain run
-  if (rideNumber === 4) {
-    if (age < 12) {
-      answerPar.textContent =
-        "The age entered is not permitted to ride the Downhill Mountain Run. It is open to those 12 and older.";
-    } else {
-      answerPar.textContent =
-        "Yes, the age entered may ride the Downhill Mountain Run. It is open to those 12 and older.";
-    }
-  }
+    // check downhill mountain run
+    case 4:
+      if (age < 12) {
+        answerPar.textContent =
+          "The age entered is not permitted to ride the Downhill Mountain Run. It is open to those 12 and older.";
+      } else {
+        answerPar.textContent =
+          "Yes, the age entered may ride the Downhill Mountain Run. It is open to those 12 and older.";
+      }
+      break;
 
-  // check the regurgitator
-  if (rideNumber === 5) {
-    if (age < 12 || age > 90) {
-      answerPar.textContent =
-        "The age entered is not permitted to ride the Regurgitator. It is open to those between the ages of 12 and 90.";
-    } else {
-      answerPar.textContent =
-        "Yes, the age entered may ride the Regurgitator. It is open to those between the ages of 12 and 90.";
-    }
+    // check the regurgitator
+    case 5:
+      if (age < 12 || age > 90) {
+        answerPar.textContent =
+          "The age entered is not permitted to ride the Regurgitator. It is open to those between the ages of 12 and 90.";
+      } else {
+        answerPar.textContent =
+          "Yes, the age entered may ride the Regurgitator. It is open to those between the ages of 12 and 90.";
+      }
+      break;
   }
 });
 
