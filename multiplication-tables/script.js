8302d8127267117b971f81988d4aeaed9779566a
Michelle Ross <miross629@gmail.com>
2023-03-30 15:54:49 -0700

    Display multiplication table for number input

---
+++ multiplication-tables/script.js
@@ -0,0 +1,37 @@
+/* get DOM elements */
+const multiplicationTableListEl = document.getElementById(
+  "multiplicationTableList"
+);
+const tableNumberInput = document.getElementById("numberInput");
+const buttonShowTable = document.getElementById("showMultTable");
+
+/* button click event listener */
+buttonShowTable.addEventListener("click", () => {
+  // convert input value to a number
+  let tableNumber = Number(tableNumberInput.value);
+
+  if (tableNumber >= 1 && tableNumber <= 12) {
+    // loop through all 12 multiplication sets and display results in list
+    for (let i = 1; i < 12; i++) {
+      // calculate the result
+      let resultNumber = tableNumber * i;
+
+      // create a result string
+      let resultString = i + " times " + tableNumber + " is " + resultNumber;
+
+      // create new list item
+      let listItem = document.createElement("li");
+
+      // set the text of the new list item to the result string
+      listItem.textContent = resultString;
+
+      // add list item to the multiplication table list
+      multiplicationTableListEl.appendChild(listItem);
+    }
+    // remove number from input
+    tableNumberInput.value = "";
+  } else {
+    return;
+    // TODO error handling
+  }
+});
