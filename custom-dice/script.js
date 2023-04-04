d59e277c53c66c9556ea1c6259942bd3a0c7b156
Michelle Ross <miross629@gmail.com>
2023-04-04 10:55:26 -0700

    On form submit display number rolled

--- custom-dice/script.js
+++ custom-dice/script.js
@@ -1,51 +1,27 @@
 /* get DOM elements */
 const form = document.getElementById("formMinMax");
+const inputMin = document.getElementById("inputMin");
+const inputMax = document.getElementById("inputMax");
 const buttonRoll = document.getElementById("buttonRoll");
-
-/* functions */
-// get number rolled
-function getNumberRolled(min, max) {
-  let range = max - min + 1;
-  let numberRolled = Math.floor(Math.random() * range) + min;
-  return numberRolled;
-}
-
-// get number from input
-function getNumberFromInput(name) {
-  // get input element
-  const input = document.getElementById(name);
-  // convert input to a number
-  let result = Number(input.value);
-  return result;
-}
-
-/* display functions */
-//  display number rolled
-function doRollDice(minInput, maxInput) {
-  let min = getNumberFromInput(minInput);
-  let max = getNumberFromInput(maxInput);
-  let number = getNumberRolled(min, max);
-
-  let message = "Rolled: " + number;
-  console.log("message", message);
-}
 
 /* events */
 // on submit display number rolled
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  // FIX display number rolled
-  // new plan:
-  // write as event listener without function calls (see TODO at end of code), commit
-  // refactor to simplify code using function calls, commit
-  doRollDice();
-});
 
-// TODO
-// steps for how to display number rolled on button click:
-// get user input for min
-// get user input for max
-// convert user input values to numbers
-// take the numbers to calculate a range
-// take that range and get a random number that represents the roll of the die
-// display a message that includes the roll of the die
+  // convert user input values to numbers
+  let min = Number(inputMin.value);
+  let max = Number(inputMax.value);
+
+  // calculate a range with min and max numbers
+  let range = max - min + 1;
+
+  // take that range and get a random number that represents the number rolled
+  let numberRolled = Math.floor(Math.random() * range) + min;
+
+  // display number rolled
+  let par = document.createElement("p");
+  let message = document.createTextNode("You rolled: " + numberRolled);
+  par.appendChild(message);
+  form.insertBefore(par, form.firstChild);
+});
