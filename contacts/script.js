d5aee5daa653cb2c6167679c54776a6a4931f600
Michelle Ross <miross629@gmail.com>
2023-04-19 11:07:22 -0700

    Organize code, add comments for clarity

--- contacts/script.js
+++ contacts/script.js
@@ -6,11 +6,15 @@ const saveButton = document.getElementById("save");
 /* events */
 // button click: find
 findButton.addEventListener("click", () => {
+  // get the name value
   let name = getElementValue("name");
+  // if name matches "Michelle", display values in form
   if (name == "Michelle") {
     displayElementValue("address", "123 Street\nCity, State Zip");
     displayElementValue("phone", "(123) 456-7890");
-  } else {
+  }
+  // if name is not "Michelle", show alert that contact is not found
+  else {
     displayNotFound();
   }
 });
@@ -20,21 +24,24 @@ saveButton.addEventListener("click", () => {
   alert("This will save contact");
 });
 
-/* functions */
-function displayElementValue(id, text) {
+/* helper functions */
+function getElementValue(id) {
+  // get the element being read
   let element = document.getElementById(id);
+  // return the value in the element
+  return element.value;
+}
+
+function displayElementValue(id, text) {
+  // get the element being changed
+  let element = document.getElementById(id);
+  // set the value in the element
   element.value = text;
 }
 
 function displayNotFound() {
   alert("Contact not found. Try searching again.");
 }
-
-function getElementValue(id) {
-  let element = document.getElementById(id);
-  return element.value;
-}
-
 /* --- * --- * --- NOTES --- * --- * --- *
 
 clo checklist
