8f5d5f1869e14eb1b1f04b1ec770a8471d87c295
Michelle Ross <miross629@gmail.com>
2023-04-27 15:57:45 -0700

    CLO, get form and its child elements

---
+++ dew-point-calculator/script.js
@@ -0,0 +1,22 @@
+/* get DOM elements */
+const formEl = document.getElementById("formCalc");
+const tempScaleEl = document.getElementById("tempScale");
+const tempNumEl = document.getElementById("tempNum");
+const humidityNumEl = document.getElementById("humidityNum");
+
+console.log("form", formEl);
+console.log("tempScale", tempScaleEl);
+console.log("tempNum", tempNumEl);
+console.log("humidityNum", humidityNumEl);
+
+/* events */
+// TODO calculate button event listener
+
+/* functions */
+
+/* --- * --- * --- NOTES --- * --- * --- *
+
+clo checklist
+[x] get form and its child elements
+
+* --- --- --- --- - * - --- --- --- --- */
