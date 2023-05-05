aa392944215c1e68dde0b405cc3019fcbc41c68e
Michelle Ross <miross629@gmail.com>
2023-05-05 15:33:13 -0700

    FIX display input element on page load

--- game-scores/script.js
+++ game-scores/script.js
@@ -16,31 +16,31 @@ window.addEventListener("load", () => {
 // TODO render a select element
 
 // render an input element
+// WIP: move some code to renderDivCtrlElement()
 function renderInputElement(element) {
+  element = dataSchema;
   // create an input element
   let inputEl = document.createElement("input");
   console.log("inputEl", inputEl);
   // TODO add attributes to input element
-  //   inputEl.setAttribute("id", element.id);
+  inputEl.setAttribute("id", element.id);
   inputEl.setAttribute("type", "text");
 
   // return input element
   return inputEl;
 }
 
-/* display function */
-function displayFormElements() {
-  console.log("Hello!");
+// TODO render div control element
 
-  // work through each of the items in the data schema
-  for (let item of dataSchema) {
-    // TODO render an element for maze select
-    // let selectElement = renderSelectElement(item);
-    // render an element for score input
-    let inputElement = renderInputElement(item);
-    // add the input and select elements to the game form
-    gameForm.appendChild(inputElement);
-  }
+/* display function */
+function displayFormElements(item) {
+  console.log("Hello!");
+  // TODO render an element for maze select
+  // let selectElement = renderSelectElement(item);
+  // render an element for score input
+  let inputElement = renderInputElement(item);
+  // add the select and input elements to the game form
+  gameForm.appendChild(inputElement);
 }
 
 /* --- * --- * --- NOTES --- * --- * --- *
@@ -66,4 +66,5 @@ stretch goal ideas
 
 debugging
 - [] fix input displaying twice
+  - Solution: Remove loop and just render input element. The loop is not needed since there is just one input to display. It's displaying twice because there are two items in the data.
 * --- --- --- --- - * - --- --- --- --- */
