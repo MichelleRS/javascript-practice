ce3c7d1767bbfe818bd733da5afa01e24030b08f
Michelle Ross <miross629@gmail.com>
2023-05-05 14:04:40 -0700

    Initial data schema planning

---
+++ game-scores/data-schema.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    id: "level",
+    label: "Level",
+    type: "select",
+    value: ["Cemetery", "Corn Maze", "Forest", "Library", "Mansion", "Museum"],
+  },
+  {
+    id: "score",
+    label: "High Score",
+    type: "input",
+  },
+];
