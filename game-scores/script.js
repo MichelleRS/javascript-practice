98162a7b68a5183dcdcf4eb99adfd7f8ceabb3a2
Michelle Ross <miross629@gmail.com>
2023-05-06 10:16:20 -0700

    WIP renderDivElement()

--- game-scores/script.js
+++ game-scores/script.js
@@ -7,9 +7,9 @@ const gameForm = document.getElementById("gameForm");
 
 /* events */
 // display form elements on page load
-window.addEventListener("load", () => {
+window.addEventListener("load", (dataSchema) => {
   // function call to display form elements
-  displayFormElements();
+  displayFormElements(dataSchema);
 });
 
 /* render functions */
@@ -30,24 +30,64 @@ function renderInputElement(element) {
   return inputEl;
 }
 
-// TODO render div control element
+// render div control element to contain a label and element tag for data scheme
+function renderDivElement(element) {
+  /* parent element */
+  // create a div element
+  let divEl = document.createElement("div");
+  // assign class of control
+  divEl.className = "control";
+
+  /* create label element */
+  let labelEl = document.createElement("label");
+  // set attribute value
+  labelEl.setAttribute("for", element.id);
+  // create text node for label
+  let text = document.createTextNode(element.label);
+  // append text node to label
+  labelEl.appendChild(text);
+
+  /* get form control by element type */
+  // initialize a variable for form control element
+  let formCtrlEl = null;
+
+  switch (element.type) {
+    case "input":
+      formCtrlEl = renderInputElement(element);
+      break;
+  }
+
+  if (formCtrlEl == null) {
+    return null;
+  }
+
+  // set attributes
+  formCtrlEl.setAttribute("id", element.id);
+  // append label and its form control to div element
+  divEl.appendChild(formCtrlEl);
+  // return div element
+  return divEl;
+}
 
 /* display function */
 function displayFormElements(item) {
   console.log("Hello!");
+  let divEl = renderDivElement(item);
   // TODO render an element for maze select
   // let selectElement = renderSelectElement(item);
   // render an element for score input
-  let inputElement = renderInputElement(item);
+  // let inputElement = renderInputElement(item);
   // add the select and input elements to the game form
-  gameForm.appendChild(inputElement);
+  gameForm.appendChild(divEl);
 }
 
 /* --- * --- * --- NOTES --- * --- * --- *
 
 clo checklist
-[x] - get displayFormElements on page load
-[x] - get inputEl on page load
+- [x] get displayFormElements
+- [x] get gameForm
+- [x] get inputEl
+- [] get divEl
 
 tasks
 initial rendering (mvp)
@@ -65,6 +105,6 @@ stretch goal ideas
 - [] add style
 
 debugging
-- [] fix input displaying twice
-  - Solution: Remove loop and just render input element. The loop is not needed since there is just one input to display. It's displaying twice because there are two items in the data.
+- [x] fix input element displaying twice
+  - Solution: Remove loop and just render input element. The loop is not needed since there is just one input to display. It was displaying twice because there are two items in data-schema.js.
 * --- --- --- --- - * - --- --- --- --- */
