b5e2bcb8fa43a7f6fa470b0750c8ad50c181eb10
Michelle Ross <miross629@gmail.com>
2023-06-14 15:06:21 -0700

    Begin refactoring with function calls

--- modular-components/script.js
+++ modular-components/script.js
@@ -5,24 +5,33 @@ import { renderComponentOne, renderComponentTwo } from "./render-utils.js";
 const modularComponentSection = document.getElementById("modularComponent");
 
 /* events */
-// TODO refactor with function calls
 window.addEventListener("load", async () => {
-  // initialize a variable to store component one elements
+  doModularComponents();
+});
+
+/* functions */
+function doModularComponents() {
+  // initialize variables to store components
   let componentOne = renderComponentOne();
-  // append component one elements to modularComponentSection
+  let componentTwo = renderComponentTwo();
+  // display component one elements
   modularComponentSection.append(componentOne);
-  // get component one button element
+  // listen for button clicks to toggle between component one and two
+  doListenForButtonClicks(componentOne, componentTwo);
+}
+
+function doListenForButtonClicks(componentOne, componentTwo) {
+  // get component one button
   let componentOneButton = document.getElementById("componentOneButton");
   // listen for component one button click
   componentOneButton.addEventListener("click", async () => {
     // remove component one elements from DOM
     modularComponentSection.removeChild(componentOne);
-    // initialize variable to store component two elements
-    let componentTwo = renderComponentTwo();
     // append component two elements to modularComponentSection
     modularComponentSection.append(componentTwo);
-    // get component two button element
+    // get component two button
     let componentTwoButton = document.getElementById("componentTwoButton");
+    console.log("componentTwoButton", componentTwoButton);
     // listen for component two button click
     componentTwoButton.addEventListener("click", async () => {
       // remove component two elements from DOM
@@ -31,6 +40,4 @@ window.addEventListener("load", async () => {
       modularComponentSection.append(componentOne);
     });
   });
-});
-
-/* functions */
+}
