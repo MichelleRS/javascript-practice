1871ac67969530351bf796b30a75a793fb738f97
Michelle Ross <miross629@gmail.com>
2023-07-14 15:48:10 -0700

    Render label and input for each data item in form

--- data-schema/render-utils.js
+++ data-schema/render-utils.js
@@ -1,7 +1,39 @@
 export function renderElement(description) {
   // create container for element
-  let div = document.createElement("div");
+  const div = document.createElement("div");
   div.setAttribute("id", "control");
+
+  /* label */
+  // create label element
+  const labelEl = document.createElement("label");
+  // append label text
+  const labelText = document.createTextNode(description.label + ":");
+  labelEl.appendChild(labelText);
+  // label element attributes
+  labelEl.setAttribute("for", description.id);
+  // append label element to container element
+  div.appendChild(labelEl);
+
+  /* input */
+  // declare input element
+  let inputEl;
+  // decide what type of input element to build
+  switch (description.type) {
+    case "input":
+      inputEl = document.createElement("input");
+      break;
+    case "textarea":
+      inputEl = document.createElement("textarea");
+      // text area attributes
+      inputEl.setAttribute("col", description.col);
+      inputEl.setAttribute("rows", description.rows);
+      break;
+  }
+  // input element attributes
+  inputEl.setAttribute("id", description.id);
+  inputEl.setAttribute("autocomplete", description.autocomplete);
+  // append input element to container element
+  div.appendChild(inputEl);
 
   // return container
   return div;
