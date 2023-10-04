39565176665788268529fb5201f122a2d2b3e055
Michelle Ross <miross629@gmail.com>
2023-10-03 17:30:13 -0700

    Search bar functionality

--- search-bar/script.js
+++ search-bar/script.js
@@ -1,28 +1,51 @@
-window.addEventListener("load", () => {
-  console.log("Hello! The page loaded.");
-});
-
 /* get DOM elements*/
 // get card template
 const cardTemplate = document.querySelector("[data-card-template]");
 // get cards container
 const cardsContainer = document.querySelector("[data-user-cards-container]");
+// get search input
+const searchInput = document.querySelector("[data-search-input]");
 
-// get user data
+/* state */
+// initialize variable to store array of users
+let users = [];
+
+// get users
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
   .then((data) => {
     // loop through data for each user
-    data.forEach((user) => {
-      // get card template
+    users = data.map((user) => {
+      // get card template and its children elements
       const card = cardTemplate.content.cloneNode(true).children[0];
-      // name
+      // get name element and set text to user name
       const nameEl = card.querySelector("[data-name]");
       nameEl.textContent = user.name;
-      // email
+      // get email element and set text to user email
       const emailEl = card.querySelector("[data-email]");
       emailEl.textContent = user.email;
       // append card to cards container
       cardsContainer.append(card);
+      // return user object with name, email, and card element
+      return { name: user.name, email: user.email, element: card };
     });
   });
+
+/* event listener */
+searchInput.addEventListener("input", (e) => {
+  // get typed value from input and convert to lower case
+  const value = e.target.value.toLowerCase();
+  // hide users that do not match the value in search input
+  users.forEach((user) => {
+    console.log("user.element", user.element);
+    // if name or email matches the value in search input, isMatch is set to true
+    const isMatch =
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value);
+    // if isMatch value is false, hide user card element
+    // if (!isMatch) {
+    //   user.element.classList.toggle("hide", true);
+    // }
+    user.element.classList.toggle("hide", !isMatch);
+  });
+});
