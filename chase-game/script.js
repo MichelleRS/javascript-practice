11cbac9e79e5a133197a48ee6255d4549dc3ed24
Michelle Ross <miross629@gmail.com>
2023-10-10 12:24:41 -0700

    Display all sprites

--- chase-game/script.js
+++ chase-game/script.js
@@ -19,18 +19,55 @@ function getImageLoadPromise(url) {
   });
 }
 
-function doLoadImage() {
+// handle multiple image promises
+async function getImages(imageUrls) {
+  // initialize an empty promise list
+  let promiseList = [];
+
+  // loop through list of image urls
+  for (let url of imageUrls) {
+    // create image load promise and add to promise list
+    promiseList[promiseList.length] = getImageLoadPromise(url);
+  }
+
+  // wait for all promises to be fulfilled
+  let result = await Promise.all(promiseList);
+  // return list of images
+  return result;
+}
+
+async function doLoadImage() {
   // get DOM element for canvas
   const canvas = document.querySelector("canvas");
+
   if (canvas.getContext) {
     // get object for drawing area
     const context = canvas.getContext("2d");
-    // get image load promise
-    let imagePromise = getImageLoadPromise("./assets/cheese.png");
-    // draw image
-    imagePromise.then((image) => context.drawImage(image, 0, 0));
-    // error handling
-    imagePromise.catch((error) => alert(error));
+    // get array of image urls
+    let imageUrls = [
+      "./assets/cheese.png",
+      "./assets/cracker.png",
+      "./assets/tomato.png",
+      "./assets/background.png",
+    ];
+
+    // get images
+    let images = await getImages(imageUrls);
+
+    // set x draw coordinate to 0
+    let x = 0;
+    // set y draw coordinate to 0
+    let y = 0;
+
+    // loop through the images
+    for (let image of images) {
+      // draw the image
+      context.drawImage(image, x, y);
+      // move x across by the width of the image
+      x = x + image.width;
+      // move y down by the height of the image
+      y = y + image.height;
+    }
   }
   // error handling
   else {
