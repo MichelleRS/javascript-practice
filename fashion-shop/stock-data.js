312f3f4a19c89f22884c79834adb5fc231ea1b67
Michelle Ross <miross629@gmail.com>
2023-10-31 11:59:55 -0700

    Log static test data for dress subclass

--- fashion-shop/stock-data.js
+++ fashion-shop/stock-data.js
@@ -1,5 +1,31 @@
 // superclass for all items the fashion shop sells
 class StockItem {
+  // method: get a random number
+  static getRandomInt(min, max) {
+    let range = max - min + 1;
+    let randomInt = Math.floor(Math.random() * range) + min;
+    return randomInt;
+  }
+
+  // method: find the largest stock reference in fashion shop
+  static getLargestStockRef(items) {
+    // handle zero stock items
+    if (items.length == 0) {
+      return 0;
+    }
+    // initialize variable for largest: set as first stock item
+    let largest = items[0].stockRef;
+    // loop through stock items
+    for (const item of items) {
+      // check if stock reference of item is larger than initialized value
+      if (item.stockRef > largest) {
+        // set item as the largest stock reference value
+        largest = item.stockRef;
+      }
+    }
+    return largest;
+  }
+
   // constructor method for shared properties
   constructor(stockRef, stockLevel, price, description, color) {
     this.stockRef = stockRef;
@@ -57,6 +83,44 @@ class Dress extends StockItem {
     // return string
     return result;
   }
+
+  // static class members for test data
+  static colors = ["terracotta", "hunter green", "coyote", "navy"];
+  static patterns = ["plain", "striped", "floral"];
+  static sizes = ["xs", "s", "m", "l", "xl"];
+
+  // method: get test data
+  static getTestItems(dest) {
+    // set stock number as one higher than the current largest stock reference in fashion shop
+    let stockNo = StockItem.getLargestStockRef(dest) + 1;
+    // loop through dress colors
+    for (let color of Dress.colors) {
+      // loop through dress patterns
+      for (let pattern of Dress.patterns) {
+        // get a random price
+        let price = StockItem.getRandomInt(40, 300);
+        // loop through dress sizes
+        for (let size of Dress.sizes) {
+          // get a random stock level
+          let stock = StockItem.getRandomInt(0, 15);
+          // build a description string
+          let description = `${color} ${pattern} dress`;
+          // create a new Dress and store it
+          dest[dest.length] = new Dress(
+            stockNo,
+            stock,
+            price,
+            description,
+            color,
+            pattern,
+            size
+          );
+          // advance the stock number
+          stockNo = stockNo + 1;
+        }
+      }
+    }
+  }
 }
 
 // subclass for pants
@@ -101,42 +165,10 @@ class Pants extends StockItem {
 // TEST
 window.addEventListener("load", () => {
   console.log("Hello from stock data!");
-  let testDress = new Dress(
-    // ref
-    221,
-    // stock level
-    7,
-    // price
-    129,
-    // description
-    "spaghetti straps",
-    // color
-    "green",
-    // pattern
-    "floral",
-    // size
-    "small"
-  );
-  console.log("testDress.getDescription()", testDress.getDescription());
-
-  // TEST pants
-  let testPants = new Pants(
-    // ref
-    222,
-    // stock level
-    2,
-    // price
-    99,
-    //description
-    "Flair",
-    // color
-    "Olive",
-    // pattern
-    "Plaid",
-    // waist
-    "25",
-    // length
-    "30"
-  );
-  console.log("testPants.getDescription()", testPants.getDescription());
+  // get test data
+  let demo = [];
+  Dress.getTestItems(demo);
+  for (let item of demo) {
+    console.log("item.getDescription()", item.getDescription());
+  }
 });
