7065c4e51b63b29fc9c6e70951a7cbccde1b5c92
Michelle Ross <miross629@gmail.com>
2023-11-01 12:44:33 -0700

    Organize and refactor render functions

--- fashion-shop/script.js
+++ fashion-shop/script.js
@@ -8,23 +8,33 @@ window.addEventListener("load", () => {
   doStartFashionShop();
 });
 
-/* functions */
-
-// function: open user interface for main page
-function openPage(title) {
-  // TODO clear previous page
-
-  // get page title
+/* render functions */
+// render page title
+function renderPageTitle(title) {
   let titleEl = document.createElement("h2");
   titleEl.innerText = title;
   titleEl.className = "pageTitle";
   mainPage.appendChild(titleEl);
 }
 
+// TODO render items for main menu navigation
+
+/* functions */
+
+// function: open user interface for main page
+function openPage(title) {
+  // TODO clear previous page
+
+  // render page title
+  renderPageTitle(title);
+}
+
 // function: display main menu
 function doShowMainMenu() {
   console.log("Hello from doShowMainMenu()!!");
+  // clear previous page and show title
   openPage("Main Menu");
+  // TODO show menu items
 }
 
 // function: start app
