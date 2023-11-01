579ef2938c9a9e6638d2159327b2ec19ac2b7059
Michelle Ross <miross629@gmail.com>
2023-11-01 11:10:50 -0700

    Render main menu title on page load

--- fashion-shop/script.js
+++ fashion-shop/script.js
@@ -1,3 +1,37 @@
+/* global variables */
+// declare variable for the html element that contains user interface
+let mainPage;
+
+/* events */
 window.addEventListener("load", () => {
-  console.log("Hello!");
+  console.log("Hello!! The page has loaded.");
+  doStartFashionShop();
 });
+
+/* functions */
+
+// function: open user interface for main page
+function openPage(title) {
+  // TODO clear previous page
+
+  // get page title
+  let titleEl = document.createElement("h2");
+  titleEl.innerText = title;
+  titleEl.className = "pageTitle";
+  mainPage.appendChild(titleEl);
+}
+
+// function: display main menu
+function doShowMainMenu() {
+  console.log("Hello from doShowMainMenu()!!");
+  openPage("Main Menu");
+}
+
+// function: start app
+function doStartFashionShop() {
+  console.log("Hello from doStartFashionShop()!!");
+  mainPage = document.getElementById("mainPage");
+  console.log("mainPage", mainPage);
+
+  doShowMainMenu();
+}
