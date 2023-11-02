decdf1c271875312a4bbecb64e2e59478ef7abcc
Michelle Ross <miross629@gmail.com>
2023-11-02 09:10:03 -0700

    TODO troubleshoot button click

--- fashion-shop/script.js
+++ fashion-shop/script.js
@@ -5,12 +5,15 @@ let mainPage;
 /* events */
 window.addEventListener("load", () => {
   console.log("Hello!! The page has loaded.");
+
   doStartFashionShop();
 });
 
 /* render functions */
 // render page title
 function renderPageTitle(title) {
+  console.log("Hello from renderPageTitle()!");
+
   let titleEl = document.createElement("h2");
   titleEl.innerText = title;
   titleEl.className = "pageTitle";
@@ -19,6 +22,8 @@ function renderPageTitle(title) {
 
 // render list of items for main menu navigation
 function renderMainMenu(schema) {
+  console.log("Hello from renderMainMenu()!!");
+
   // create a container for list
   let listEl = document.createElement("ul");
   // add class name to list element
@@ -37,9 +42,10 @@ function renderMainMenu(schema) {
     // add class name to button
     buttonEl.className = "mainMenuItemButton";
     // TODO add on click function to button
-
+    buttonEl.setAttribute("onclick", listItem.func);
     // append button to list item element
     listItemEl.appendChild(buttonEl);
+
     // append list item element to list
     listEl.appendChild(listItemEl);
   }
@@ -47,10 +53,22 @@ function renderMainMenu(schema) {
   mainPage.appendChild(listEl);
 }
 
+/* functions: click events for main menu buttons */
+// add dress
+function doAddDress() {
+  console.log("I clicked Add Dress!!");
+}
+
+// add pants
+function doAddPants() {
+  console.log("I clicked Add Pants!!");
+}
+
 /* functions */
 
 // function: open user interface for main page
 function openPage(title) {
+  console.log("Hello from openPage()!!");
   // TODO clear previous page
 
   // render page title
@@ -66,14 +84,17 @@ function doShowMainMenu() {
   renderMainMenu([
     {
       label: "Add Dress",
+      func: "doAddDress()",
     },
     {
       label: "Add Pants",
+      func: "doAddPants()",
     },
   ]);
 }
 
 // function: start app
+
 function doStartFashionShop() {
   console.log("Hello from doStartFashionShop()!!");
   mainPage = document.getElementById("mainPage");
